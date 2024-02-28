5113208fcb6f1a878378271d658672d0b884bbb1
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-02-27 21:08:53 -0300

    adding sqrt function using the secant approximation method

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod numeric_methods;
 mod functions;
 
-use numeric_methods::function_roots::bisec_method;
+use numeric_methods::function_roots::{
+    bisec_method,
+    secant_method,
+    sqrt,
+};
 use functions::f;
 
-use crate::numeric_methods::function_roots::secant_method;
 
 fn main() {
     let x_bisec = bisec_method(0.0, 10.0, 1e-12).unwrap_or(0.0);
     let x_secant = secant_method(0.0, 10.0, 1e-11).unwrap_or(0.0);
     println!("x = {} f(x) = {}", x_bisec, f(x_bisec));
     println!("x = {} f(x) = {}", x_secant, f(x_secant));
+
+    println!("sqrt(144) = {}", sqrt(144.0).unwrap());
 }
--- src/numeric_methods/function_roots.rs
+++ src/numeric_methods/function_roots.rs
@@ -19,15 +19,15 @@ pub fn bisec_method(mut a: f64, mut b: f64, epsilon: f64) -> Option<f64>
         return None;
     }
 
-    let mut c: f64;
+    let mut f_root: f64;
 
     loop {
-        c = (b + a) / 2.0;
+        f_root = (b + a) / 2.0;
 
-        if f(c) < 0.0 {
-            a = c;
+        if f(f_root) < 0.0 {
+            a = f_root;
         } else {
-            b = c;
+            b = f_root;
         }
 
         if (b - a) / 2.0 < epsilon {
@@ -35,26 +35,57 @@ pub fn bisec_method(mut a: f64, mut b: f64, epsilon: f64) -> Option<f64>
         }
     }
 
-    Some(c)
+    Some(f_root)
 }
 
 
 pub fn secant_method(mut a: f64, mut b: f64, epsilon: f64) -> Option<f64>
 {
-    let mut c: f64;
+    let mut f_root: f64;
 
     loop {
         let alpha = (f(b) - f(a)) / (b - a); // linear coefficient
-        let beta  = f(a) - alpha * a;
-        c = -beta / alpha;
+        let beta  = f(a) - alpha * a; // x = a -> f(a) = alpha * a + beta -> beta = f(a) - alpha * a
+        f_root = -beta / alpha; // alpha*x+beta = 0 -> x = -beta / alpha 
 
         b = a;
-        a = c;
+        a = f_root;
 
         if f64::abs(b - a) / f64::max(1 as f64, f64::abs(b)) < epsilon {
             break;
         }
     }
 
-    Some(c)
+    Some(f_root)
+}
+
+
+pub fn sqrt(n: f64) -> Option<f64>
+{
+    if n < 0.0 {
+        println!("There is no square root for negative number!");
+        return None;
+    }
+
+    let mut a = 0.0;
+    let mut b = n;
+    let epsilon = 1e-14;
+    let mut root: f64;
+
+    let f_sqrt: fn(f64, f64) -> f64 = |x:f64, n:f64| x*x - n;
+
+    loop {
+        let alpha = (f_sqrt(b, n) - f_sqrt(a, n)) / (b - a);
+        let beta  = f_sqrt(a, n) - alpha * a;
+        root = -beta / alpha;
+
+        b = a;
+        a = root;
+
+        if f64::abs(b - a) / f64::max(1.0, f64::abs(b)) < epsilon {
+            break;
+        }
+    }
+
+    Some(root)
 }
