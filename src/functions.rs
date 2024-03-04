2494949e982e8fa02fb1484d562f4fedc767ccd7
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-03-04 20:59:41 -0300

    adding derivative function

--- src/functions.rs
+++ src/functions.rs
@@ -2,3 +2,9 @@ pub fn f(x: f64) -> f64
 {
     f64::exp(x) + f64::sin(x*x) - 10.0
 }
+
+pub fn derivative(f: fn(f64) -> f64, x: f64) -> f64
+{
+    let delta_x = 1e-10;
+    (f(x + delta_x) - f(x)) / delta_x
+}
