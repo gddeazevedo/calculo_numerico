adf7d4f8bedd0f2389dac2cdd51c310ce8275928
gddeazevedo <gabrielcapuerj@gmail.com>
2024-03-19 13:15:04 -0300

    adding partial pivoting gaussian elimination, gaussian elimination, gaussian solver and gaussian compact solver

--- src/exact_methods/linear_systems_solvers.rs
+++ src/exact_methods/linear_systems_solvers.rs
@@ -1,3 +1,5 @@
+use crate::types::Matrix;
+
 /**
  * 
  * a must be a square matrix where a[i][j] == 0 for i < j
@@ -5,7 +7,7 @@
  * this functions returns x, which is the variables vector, solution vector
  * starts with the first item and goes until the diagonal item
  */
-pub fn solve_inf(a: &Vec<Vec<f64>>, b: &Vec<f64>) -> Vec<f64>
+pub fn solve_inf(a: &Matrix<f64>, b: &Vec<f64>) -> Vec<f64>
 {
     let n = b.len();
     let mut x = vec![0.0; n];
@@ -33,7 +35,7 @@ pub fn solve_inf(a: &Vec<Vec<f64>>, b: &Vec<f64>) -> Vec<f64>
  * this functions returns x, which is the variables vector, solution vector
  * starts with the diagonal item and goes until the last item
  */
-pub fn solve_sup(a: &Vec<Vec<f64>>, b: &Vec<f64>) -> Vec<f64>
+pub fn solve_sup(a: &Matrix<f64>, b: &Vec<f64>) -> Vec<f64>
 {
     let n = b.len();
     let mut x = vec![0.0; n];
@@ -58,7 +60,7 @@ pub fn solve_sup(a: &Vec<Vec<f64>>, b: &Vec<f64>) -> Vec<f64>
  * Returns the vector that results from the product between
  * a matrix n x n and a vector of dimension n
  */
-pub fn matvec(a: &Vec<Vec<f64>>, v: &Vec<f64>) -> Vec<f64>
+pub fn matvec(a: &Matrix<f64>, v: &Vec<f64>) -> Vec<f64>
 {
     let mut x: Vec<f64> = vec![0.0; v.len()];
     let n = v.len();
@@ -80,7 +82,7 @@ pub fn matvec(a: &Vec<Vec<f64>>, v: &Vec<f64>) -> Vec<f64>
 /**
  * Return the product of two matrices
  */
-pub fn matmat(a: &Vec<Vec<f64>>, b: &Vec<Vec<f64>>) -> Vec<Vec<f64>>
+pub fn matmat(a: &Matrix<f64>, b: &Matrix<f64>) -> Matrix<f64>
 {
     let n = a.len();
     let mut c = vec![vec![0.0; n]; n];
@@ -104,10 +106,11 @@ pub fn matmat(a: &Vec<Vec<f64>>, b: &Vec<Vec<f64>>) -> Vec<Vec<f64>>
 
 /**
  * Decompose a square matrix A (n x n) into two square triangular matrices
- * L (lower triangular matrix)
- * U (upper triangular matrix)
+ * Returns:
+ *  - L (lower triangular matrix)
+ *  - U (upper triangular matrix)
  */
-pub fn lu_decomp(a: &Vec<Vec<f64>>) -> (Vec<Vec<f64>>, Vec<Vec<f64>>)
+pub fn lu_decomp(a: &Matrix<f64>) -> (Matrix<f64>, Matrix<f64>)
 {
     let n = a.len();
 
@@ -146,11 +149,119 @@ pub fn lu_decomp(a: &Vec<Vec<f64>>) -> (Vec<Vec<f64>>, Vec<Vec<f64>>)
 
 
 /**
- * 
+ * Solve a square linear system of order n using de decomposition LU
+ * A = LU
+ * LUx = b
+ * Ly = b
+ * Ux = y
  */
-pub fn lu_solver(a: &Vec<Vec<f64>>, b: &Vec<f64>) -> Vec<f64>
+pub fn lu_solver(a: &Matrix<f64>, b: &Vec<f64>) -> Vec<f64>
 {
     let (l, u) = lu_decomp(a);
     let y = solve_inf(&l, b);
-    solve_sup(&u, &y)
+    solve_sup(&u, &y) // x
+}
+
+
+/**
+ * Gaussian elimination method to create an upper triangular matrix
+ */
+pub fn gaussian_elimination(a: &Matrix<f64>, b: &Vec<f64>) -> (Matrix<f64>, Vec<f64>)
+{
+    let mut a_ = a.clone();
+    let mut b_ = b.clone();
+    let n = b.len();
+
+    for k in 0..(n - 1) {
+        for i in (k + 1)..n {
+            let p = a_[i][k] / a_[k][k];
+            b_[i] = b_[i] - b_[k] * p;
+
+            for j in k..n {
+                a_[i][j] = a_[i][j] - a_[k][j] * p;
+            }
+        }
+    }
+
+    (a_, b_)
+}
+
+
+/**
+ * Chooses the element with greatest absolute value as pivot of a column
+ */
+fn choose_best_pivot(a: &mut Matrix<f64>, b: &mut Vec<f64>, k: usize) {
+    let n = b.len();
+    let mut max_element = f64::abs(a[k][k]);
+    let mut max_index   = k;
+
+    for line in (k + 1)..n {
+        if f64::abs(a[line][k]) > max_element {
+            max_element = f64::abs(a[line][k]);
+            max_index   = line;
+        }
+    }
+
+    if k != max_index {
+        // swaps the line k with the line max_index which contains the max pivot
+
+        let tmp = a[k].clone();
+        a[k] = a[max_index].clone();
+        a[max_index] = tmp;
+
+        let tmp = b[k];
+        b[k] = b[max_index];
+        b[max_index] = tmp;
+    }
+}
+
+
+/**
+ * Gaussian elimination method to create an upper triangular matrix, using partial pivot
+ */
+pub fn partial_pivot_gaussian_elimination(a: &Matrix<f64>, b: &Vec<f64>) -> (Matrix<f64>, Vec<f64>)
+{
+    let mut a_ = a.clone();
+    let mut b_ = b.clone();
+    let n = b.len();
+
+    for k in 0..(n - 1) {
+        choose_best_pivot(&mut a_, &mut b_, k);
+
+        for i in (k + 1)..n {
+            let p = a_[i][k] / a_[k][k];
+            b_[i] = b_[i] - b_[k] * p;
+
+            for j in k..n {
+                a_[i][j] = a_[i][j] - a_[k][j] * p;
+            }
+        }
+
+    }
+
+    (a_, b_)
+}
+
+
+/**
+ * Solves linear systems of order n using the gaussian elimination
+ * partial_pivot controls whether the algorithm is going to use partial pivoting or not
+ */
+pub fn gaussian_solver(a: &Matrix<f64>, b: &Vec<f64>, partial_pivot: bool) -> Vec<f64>
+{   
+    let (a_, b_) = if partial_pivot {
+        gaussian_elimination(a, b)
+    } else {
+        partial_pivot_gaussian_elimination(a, b)
+    };
+
+    solve_sup(&a_, &b_)
+}
+
+
+pub fn gaussian_compact_solver(a: &mut Matrix<f64>) -> Vec<f64>
+{
+    let mut x = vec![0.0; a.len()];
+
+    x
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![allow(dead_code, unused)]
 
 mod numeric_methods;
-mod functions;
+mod helpers;
 mod exact_methods;
+mod types;
 
 use exact_methods::linear_systems_solvers::{
-    solve_inf, solve_sup, matvec, matmat,
-    lu_solver,
+    gaussian_solver, lu_decomp, lu_solver, matmat, matvec, solve_inf, solve_sup
 };
+
+use crate::helpers::get_identity_matrix;
 
 
 fn main() {
@@ -37,7 +39,30 @@ fn main() {
     ];
 
     let b = vec![0.0, -7.0, -5.0];
-    let x = lu_solver(&a, &b);
+    let x = gaussian_solver(&a, &b, true);
 
-    println!("{:?}", x);
+    println!("x = {:?} (Gaussian solver partial pivot)", x);
+
+    let x = lu_solver(&a, &b);
+    println!("x = {:?} (LU solver)", x);
+
+    let a = vec![
+        vec![6.0, 2.0, -1.0],
+        vec![2.0, 4.0,  1.0],
+        vec![3.0, 2.0,  8.0],
+    ];
+
+
+    let b = vec![7.0, 7.0, 13.0];
+
+    // let x: Vec<i32> = gaussian_solver(&a, &b, false)
+    //     .into_iter()
+    //     .map(|xi| f64::round(xi) as i32)
+    //     .collect();
+
+    let x = gaussian_solver(&a, &b, false);
+    println!("x = {:?} (Gaussian solver no partial pivot)", x);
+
+    let x = gaussian_solver(&a, &b, true);
+    println!("x = {:?} (Gaussian solver partial pivot)", x);
 }
