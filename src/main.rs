de9a644b200aaa610e4f879048cfd0a414e80f12
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-04-30 09:43:25 -0300

    adding method to find eigen values

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 #![allow(dead_code, unused)]
 
+use eigen::eigen_values::{leverrier, tr};
 use linear_systems::{exact_methods::cholesky_solver, helpers::{cholesky_method, print_matrix, transpose}, iterative_methods::{gauss_seidel_solver, jacobi_richardson_solver}};
 use numeric_methods::function_roots::{linear_iteration_method, regula_falsi};
 
-use crate::numeric_methods::function_roots::newton_method;
+use crate::{linear_systems::helpers::matmat, numeric_methods::function_roots::newton_method};
 
 mod numeric_methods;
 mod linear_systems;
+mod eigen;
 mod types;
 
 
 fn main() {
-    let f = |x: f64| f64::exp(1.0833 * x) - 5.*x*x;
-    let x = newton_method(f, 3.5);
-    println!("x = {x}");
-    println!("f({x}) = {}", f(x));
+    let a = vec![
+        vec![1., 1., -1.],
+        vec![0., 0., 1.],
+        vec![-1., 1., 0.]
+    ];
+    let p = leverrier(&a);
+    println!("{:?}", p);
 }
