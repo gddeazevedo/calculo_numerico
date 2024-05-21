46d3869cdcd7f2ea63504f8af9bfb11aa0924b98
gddeazevedo <gabrielcapuerj@gmail.com>
2024-05-21 20:17:11 -0300

    testing the power method

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 #![allow(dead_code, unused)]
 
-use eigen::eigen_values::{leverrier, tr};
-use linear_systems::{exact_methods::cholesky_solver, helpers::{cholesky_method, print_matrix, transpose}, iterative_methods::{gauss_seidel_solver, jacobi_richardson_solver}};
+
+use linear_systems::{eigen::power_method, exact_methods::cholesky_solver, helpers::{cholesky_method, infinity_vecnorm, print_matrix, transpose}, iterative_methods::{gauss_seidel_solver, jacobi_richardson_solver}};
 use numeric_methods::function_roots::{linear_iteration_method, regula_falsi};
 
-use crate::{linear_systems::helpers::matmat, numeric_methods::function_roots::newton_method};
+use linear_systems::eigen::leverrier;
+
+use crate::linear_systems::helpers::scalar_mul;
 
 mod numeric_methods;
 mod linear_systems;
-mod eigen;
 mod types;
 
 
 fn main() {
     let a = vec![
-        vec![1., 1., -1.],
-        vec![0., 0., 1.],
-        vec![-1., 1., 0.]
+        vec![3., 0., 1.0],
+        vec![2., 2., 2.],
+        vec![4., 2., 5.]
     ];
-    let p = leverrier(&a);
-    println!("{:?}", p);
+
+    let l = power_method(&a);
+    println!("Largest eigenvalue: {}", l);
 }
