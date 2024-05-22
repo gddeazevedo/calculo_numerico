a3bfabe09f744b7b6be55a2d4a0c16865c92b739
gddeazevedo <gabrielcapuerj@gmail.com>
2024-05-21 22:17:10 -0300

    changing dir name

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
 #![allow(dead_code, unused)]
 
-
-use linear_systems::{eigen::power_method, exact_methods::cholesky_solver, helpers::{cholesky_method, infinity_vecnorm, print_matrix, transpose}, iterative_methods::{gauss_seidel_solver, jacobi_richardson_solver}};
-use numeric_methods::function_roots::{linear_iteration_method, regula_falsi};
-
-use linear_systems::eigen::leverrier;
-
-use crate::linear_systems::helpers::scalar_mul;
-
 mod numeric_methods;
-mod linear_systems;
+mod linear_algebra;
 mod types;
 
 
 fn main() {
-    let a = vec![
-        vec![3., 0., 1.0],
-        vec![2., 2., 2.],
-        vec![4., 2., 5.]
-    ];
 
-    let l = power_method(&a);
-    println!("Largest eigenvalue: {}", l);
 }
