8c18536f1edbb6c9925ead18c835f7c25b091038
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-08-17 17:19:26 -0300

    fixing comments

--- src/calculus/integration.rs
+++ src/calculus/integration.rs
@@ -1,5 +1,5 @@
 /**
- * Starts the numeric integration from the left side of the rectangle.
+ * Starts the numeric integration from a rectangle that starts from the left side of the point.
  * x[i] = a + i * delta_x
  */
 pub fn integrate_left(f: fn(f64) -> f64, a: f64, b: f64, n: u32) -> f64
@@ -18,7 +18,7 @@ pub fn integrate_left(f: fn(f64) -> f64, a: f64, b: f64, n: u32) -> f64
 
 
 /**
- * Starts the numeric integration from the right side of the rectangle.
+ * Starts the numeric integration from a rectangle that starts from the left side of the point.
  * x[i] = a + (i + 1) * delta_x
  */
 pub fn integrate_right(f: fn(f64) -> f64, a: f64, b: f64, n: u32) -> f64
@@ -36,7 +36,7 @@ pub fn integrate_right(f: fn(f64) -> f64, a: f64, b: f64, n: u32) -> f64
 }
 
 /**
- * Starts the numeric integration from the middle of the rectangle.
+ * Starts the numeric integration from a rectangle that starts from the middle of the point.
  * x[i] = a + (i + 0.5) * delta_x
  */
 pub fn integrate_middle(f: fn(f64) -> f64, a: f64, b: f64, n: u32) -> f64
@@ -75,7 +75,7 @@ pub fn integrate_trapezoid(f: fn(f64) -> f64, a: f64, b: f64, n: u32) -> f64
 
 /**
  * Integrates the function f using the one third Simpon method
- * (h / 3) * ( f( x0 ) + f( x2N ) +  4*sum( f( x2i-1 ), i, N ) + 2 * sum( f( x2i ), 1, N - 1 ) )
+ * (h / 3) * ( f( x0 ) + f( x2N ) +  4*sum( f( x2i-1 ), 1, N ) + 2 * sum( f( x2i ), 1, N - 1 ) )
  * h = ( x2n - x0 ) / 2N 
  */
 pub fn integreate_simpson(f: fn(f64) -> f64, x0: f64, x2n: f64, n: u32) -> f64 {
